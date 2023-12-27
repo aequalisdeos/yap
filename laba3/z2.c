ade5c2c9dbb7dd376590cf03e66b9a366d213a28
aequalisdeos <116069187+aequalisdeos@users.noreply.github.com>
2023-12-28 02:21:39 +0500

    Add files via upload

---
+++ laba3/z2.c
@@ -0,0 +1,6 @@
+// #include <stdio.h>
+// extern int yyparse();  // Объявляем функцию yyparse из файла, сгенерированного Bison
+// int main() {
+//     yyparse();  // Запускаем парсер
+//     return 0;
+// }
